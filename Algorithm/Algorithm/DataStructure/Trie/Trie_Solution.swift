0db2d9493db694cc1af4bd4d9e749f94f409f0b3
dong_shuai_wen <dongshuaiwenchina@163.com>
2023-11-03 17:39:26 +0800

    数据结构7 - 字典树Trie ：二维网格中的单词搜索问题

--- Algorithm/Algorithm/DataStructure/Trie/Trie_Solution.swift
+++ Algorithm/Algorithm/DataStructure/Trie/Trie_Solution.swift
@@ -82,4 +82,31 @@ class Trie {
         }
         return true
     }
+    
+    
+    // MARK: 2、单词搜索 II https://leetcode.cn/problems/word-search-ii/
+        /* 未完成
+         给定一个 m x n 二维字符网格 board 和一个单词（字符串）列表 words， 返回所有二维网格上的单词 。
+         单词必须按照字母顺序，通过 相邻的单元格 内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母在一个单词中不允许被重复使用。
+         */
+    func findWords(_ board: [[Character]], _ words: [String]) -> [String] {
+
+        return findWords_DFS(board, words)
+        
+        return findWords_Trie(board, words)
+    }
+    
+    /* 解法1：DFS 深度优先遍历
+     */
+    func findWords_DFS(_ board: [[Character]], _ words: [String]) -> [String] {
+
+        return []
+    }
+    
+    /* 解法2：Trie 字典树
+     */
+    func findWords_Trie(_ board: [[Character]], _ words: [String]) -> [String] {
+
+        return []
+    }
 }
